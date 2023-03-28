45e6af9cbad438fe893a1ace945155d1f6fa7ec3
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2023-03-28 14:56:12 +0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './assets/scss/app.scss';
 import Navbar from './components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import News from './pages/News';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/News" element={<News />} ></Route>
+        <Route path="/Contact" element={<Contact />} ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/News.js
@@ -0,0 +1,6 @@
+function News() {
+  return (
+    <h2>NewsPage</h2>
+  )
+};
+export default News;
