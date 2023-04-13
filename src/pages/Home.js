ce921066e35e2c578acb1ea949d96e272a7ea069
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2023-04-13 16:04:21 +0700

    add fontawesome

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
     <div className="pageContainer homePage">
+      <div className="container">
+         <FontAwesomeIcon icon={faCircleXmark} />
+      </div>
       <h2>HomePage</h2>
     </div>
   )
